ba3fe96f8e90f01ef3a34b76fe6bf5b4e1b636c2
Keene23aZ4 <kentoyamaza17@gmail.com>
2025-10-28 22:55:13 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+from utils.pose_extractor import extract_pose
+from utils.feature_engineering import extract_features
+from utils.model_loader import load_model
+
+model = load_model()
+uploaded_file = st.file_uploader("動画をアップロード", type=["mp4"])
+if uploaded_file:
+    video_path = save_temp_video(uploaded_file)
+    pose_array = extract_pose(video_path)
+    features = extract_features(pose_array)
+    prediction = model.predict(features)
+    st.write(f"フォーム評価（最終フレーム）: {prediction[-1]}")
